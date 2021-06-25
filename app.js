243e3ecf44893fffb77e3a12a7de07e490241c58
FernandoHazel <A01229544@itesm.mx>
2021-06-25 16:09:21 -0500

    el servidor ya navega entre las diferentes páginas y añadí una hoja de estilos para todos sin borraar las que teníamos

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ app.listen(PORT, ()=>{
     console.log("Server running on http://localhost:"+PORT)
 })
 
+//creando una dirección estática
+app.use('/static', express.static(__dirname +'/Public'))
+app.use('/test', express.static(__dirname +'/Test'))
+
 // pagina de inicio
-app.get("/", (req, res)=>{
-    res.send("Bienvenido a TechnoShop :)")
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/Views/home.html')
+})
+
+app.get('/carrito', (req, res)=>{
+    res.sendFile(__dirname + '/Views/carrito.html')
+})
+
+app.get('/detalles', (req, res)=>{
+    res.sendFile(__dirname + '/Views/detalles.html')
+})
+
+app.get('/test', (req, res)=>{
+    res.sendFile(__dirname + '/Test/footer_header.html')
 })
