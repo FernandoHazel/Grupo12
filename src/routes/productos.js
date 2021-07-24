fed91c1cd4be9bab4029f6aa7e126605c13af460
FernandoHazel <A01229544@itesm.mx>
2021-07-23 21:11:16 -0500

    añadida ruta de creación de producto

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,15 +1,26 @@
 const express = require("express")
 const productosController = require("../controllers/productosController")
-
+const multerIMG = require('../config/multerIMG')
 
 const router = express.Router()
 
 router.get("/", (req, res)=>{
    res.send("Productos")
 })
-router.get("/crear", productosController.crear)
+
 router.get("/detalles/:id?", productosController.detalles)
-router.get("/editar/:id?", productosController.editar)
+
+//*******crear producto ********/
+router.get("/crear", productosController.crear)
+router.post("/crear", multerIMG.single('img'), productosController.store)
+
+//*******editar producto ********/
+router.get("/editar/:id?", productosController.editor)
+router.put("/editar/:id?", productosController.editar)
+
+//*******borrar producto ********/
+router.delete("/borrar/:id?", productosController.borrar)
+
 router.get("/categoria/:id?", productosController.categoria)
 router.get("/all", productosController.all)
 router.get("/offerts", productosController.offerts)
