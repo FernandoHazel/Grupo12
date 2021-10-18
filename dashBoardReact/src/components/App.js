014762ff2340d4fda088d22a480c1a0905d3488d
BlackFireBlast <skyblue722@hotmail.com>
2021-10-18 14:57:45 -0500

    dashBoardReact

---
+++ dashBoardReact/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SideBar from './SideBar';
+import ContentWrapper from './ContentWrapper';
+import Prueba from './Prueba';
+function App() {
+  return (
+    <React.Fragment>
+      	<div id="wrapper">
+          <SideBar />
+          <Prueba />
+
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
