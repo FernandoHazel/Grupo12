63a4830e56e17bc0a036c85a45c312de49a01293
BlackFireBlast <skyblue722@hotmail.com>
2021-10-19 18:01:28 -0500

    avance en categoriesInDb

--- dashBoardReact/src/components/CategoriesInDb.js
+++ dashBoardReact/src/components/CategoriesInDb.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function CategoriesInDb() {
+  let array=['Acción','Animación','Aventura','Ciencia Ficción']
+
+  const [category,setCategory]=useState([])
+
+  useEffect(()=>{
+    fetch('api/products/all')
+      .then(respuesta =>{
+        return respuesta.json()
+      })
+      .then(products =>{
+       let lengthCategory=Object.keys(products.countByCategory).length
+       let categories=products.countByCategory
+       console.log('q='+lengthCategory)
+
+       let key
+       let valor
+       let array=[]
+        for(let i=0;i<lengthCategory;i++){
+          //setCategory([category,Object.keys(categories)[i]  ])
+          console.log('dentrodelFOr')
+           key=Object.keys(categories)[i]
+           valor=Object.values(categories)[i]
+          console.log(key)//nombre de categoria
+          console.log(valor)
+          array.push({key:key,valor:valor})
+        }
+      console.log(array)
+        setCategory(array)
+
+      })
+      .catch(error=>console.log(error))
+
+
+  },[])
+  //lengthCategory=Object.keys(category[0]).length
+  //console.log(Object.keys(category[0])[0])
+  console.log('categoryArray')
+  console.log(category)
   return (
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
@@ -11,61 +49,22 @@ function CategoriesInDb() {
         </div>
         <div className="card-body">
           <div className="row">
+
+
+           { category.map((c) => {
+             return(
             <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
+                <div className="card-body">{c.key} (hay {c.valor} productos)</div>
               </div>
             </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
+             )
+            })}
+
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default CategoriesInDb;
